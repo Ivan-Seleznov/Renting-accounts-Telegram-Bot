5f5f238b1d42f9244c1cd16084aba5d1fcedbc7b
General <General@DESKTOP-9OCKJJ6>
2021-12-05 18:03:08 +0200

    Добавлен класс для работы с ботом. Добавлен метод инициализации переменных в классе TelegramBot

--- ps_rent_bot/Program.cs
+++ ps_rent_bot/Program.cs
@@ -1,13 +1,21 @@
-﻿namespace ps_rent_bot
+﻿using Telegram.Bot;
+namespace ps_rent_bot
 {
     class Program
     {
         //Бот по аренде игр на playstation4 \ ps5
         //ps_rent_bot
         //Tocken: 5057979799:AAFXniv23yLZq-orze4eyQqCidhC9I9TtHY
+        public TelegramBotClient client { get; set; }
+        
         static void Main()
         {
             Console.WriteLine("ps_rent_bot\n 5057979799:AAFXniv23yLZq-orze4eyQqCidhC9I9TtHY");
+            
+        }
+        void StartBot()
+        {
+            
         }
     }
 }
---
+++ ps_rent_bot/TelegramBot.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace ps_rent_bot
+{
+    class TelegramBot
+    {
+        public TelegramBotClient client { get; set; }
+        public TelegramBot()
+        {
+           
+        }
+        public string BotName { get; private set; }
+        public string BotUsername { get; private set; }
+        public string BotDescription { get; private set; }
+        public string BotTocken { get; private set; }
+        public string PathToTockenFile { get; set; }
+        public string PathToDescriptionFile { get; set; }
+
+        private void StartBot()
+        {
+            client.StartReceiving();
+            Console.WriteLine($"Бот: {BotName} .\nBot Username: {BotUsername}\n\tЗапущен.");
+            
+            client.StopReceiving();
+        }
+        private void Inizialize(string PathToTockenFile, string PathToDescriptionFile)
+        {
+            Console.WriteLine("Инициализация...");
+            BotName = "Бот по аренде игр на playstation4 \\ ps5";
+            BotUsername = "ps_rent_bot";
+
+            try
+            {
+                if (PathToTockenFile != null)
+                {
+                    BotTocken = File.ReadAllText(PathToTockenFile);
+                }
+                else
+                {
+                    throw new Exception(message: "В PathToTockenFile null");
+                }
+                Console.WriteLine("Токен инициализирован");
+            }
+            catch (Exception ex) { Console.WriteLine("Ошибка инициализации токена.\n" + ex.Message); }
+            try
+            {
+                if (PathToDescriptionFile != null)
+                {
+                    BotDescription = File.ReadAllText(PathToDescriptionFile);
+                }
+                else
+                {
+                    throw new Exception(message: "В PathToDescriptionFile null");
+                }
+                Console.WriteLine("Токен инициализирован");
+            }
+            catch (Exception ex) { Console.WriteLine("Ошибка инициализации описания бота.\n" + ex.Message); }
+        }
+        private void ConsoleComands()
+        {
+            while (true)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    default:
+                        Console.WriteLine("Введите команду. Для просмотра всего списка комманд используйте /help");
+                        break;
+                }
+            }
+        }
+    }
+}
