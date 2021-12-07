5efe10277dfbafec4ffa95e370be3b8333f5eb20
General <General@DESKTOP-9OCKJJ6>
2021-12-07 15:18:28 +0200

    Убран ЗАКОМЕНТИРОВАНЫЙ ТОКЕН

--- ps_rent_bot/Program.cs
+++ ps_rent_bot/Program.cs
@@ -3,9 +3,7 @@ namespace ps_rent_bot
 {
     class Program
     {
-        //Бот по аренде игр на playstation4 \ ps5
-        //ps_rent_bot
-        //Tocken: 5057979799:AAFXniv23yLZq-orze4eyQqCidhC9I9TtHY        
+               
         static void Main()
         {
             //TelegramBot telegramBot = new TelegramBot(@"BotInfo\tocken.txt", @"BotInfo\description.txt", true);
